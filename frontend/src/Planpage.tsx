7fb8a1d595fe4d8e1846b720f4ef120c220e80fa
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2022-10-28 06:25:38 +0400

    step page v0.1

--- frontend/src/Planpage.tsx
+++ frontend/src/Planpage.tsx
@@ -1,5 +1,4 @@
 import { createSignal, createResource, createEffect, For, Accessor } from "solid-js";
-import { createStore } from "solid-js/store";
 
 
 async function fetchThemes() {
@@ -10,18 +9,23 @@ async function fetchSteps() {
     return (await fetch('http://127.0.0.1:8000/api/v1/step/')).json()
 }
 
-async function fetchStepsByTheme(theme_id: any) {
-    return (await fetch(`http://127.0.0.1:8000/api/v1/step/${theme_id}/steps_by_theme/`)).json()
-}
-
-
+const fetchStep = async (stepid: number) =>
+    (await fetch(`http://127.0.0.1:8000/api/v1/step/${stepid}/`)).json();
 
 
 const Planpage = () => {
-    const [themesList] = createResource(fetchThemes);
-    const [stepsList] = createResource(fetchSteps);
     const [themes, setThemes] = createSignal();
     const [steps, setSteps] = createSignal();
+    const [stepId, setStepId] = createSignal(1);
+    const [themesList] = createResource(fetchThemes);
+    const [stepsList] = createResource(fetchSteps);
+    const [step] = createResource(stepId, fetchStep);
+
+    const toggle = (e: any) => {
+        const stepid = parseInt(e.target.dataset.stepid);
+        setStepId(stepid);
+    }
+
 
     createEffect(() => {
         if (themesList() && stepsList()) {
@@ -34,8 +38,7 @@ const Planpage = () => {
 
     return (
         <main class="d-flex">
-
-            <div class="flex-shrink-0 p-3 bg-dark" style="width: 150px;">
+            <div class="flex-shrink-0 p-3 bg-dark" style="width: 200px;">
                 <a class="d-flex align-items-center pb-3 mb-3 link-light text-decoration-none border-bottom">
                     <span class="fs-2 fw-semibold">Plan</span>
                 </a>
@@ -47,28 +50,42 @@ const Planpage = () => {
                             </button>
 
                             <div class="collapse" id={theme.title}>
+                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
 
-                                <For each={steps()}>
-                                    {(step: any, index: Accessor<number>) => {
-                                        if (step.theme == theme.id){
-                                        return <div><ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                                         </ul>
-                                           <li><a href={"#" + step.title} class="link-light d-inline-flex text-decoration-none rounded">{step.title}</a></li> 
-                                        </div>
-                                    }}}
-                                </For>
-
+                                    <For each={steps()}>
+                                        {(step: any, index: Accessor<number>) => {
+                                            if (step.theme == theme.id) {
+                                                return <>
+                                                    <button class="btn d-inline-flex align-items-center rounded border-0" onClick={toggle}>
+                                                        <a href={"#" + step.title} data-stepid={step.id} class="link-light d-inline-flex text-decoration-none rounded">
+                                                            {step.title} {step.id}
+                                                        </a>
+                                                    </button>
+                                                </>
+                                            }
+                                        }}
+                                    </For>
+                                </ul>
                             </div>
                         </li>
                     }</For>
                 </ul>
+            </div>
+            <div>
+                {step() && (
+                    <section>
+                        <h1>{step().title}</h1>
+                        <p>{step().theme}</p>
+                        <p>{step().description}</p>
+                    </section>
+                )}
             </div>
         </main>
 
     );
 };
 
-export default Planpage; 
+export default Planpage;
 
 
 
