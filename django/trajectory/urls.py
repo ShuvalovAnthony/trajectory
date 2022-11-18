abddd0f15b39cddf68c7dd16a045b4347c83a52f
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2022-11-18 23:13:44 +0400

    prod ready

--- django/trajectory/urls.py
+++ django/trajectory/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic.base import TemplateView
-
+from django.conf import settings
+from django.conf.urls.static import static
 from main.views import *
 from rest_framework import routers
 
@@ -20,4 +21,5 @@ urlpatterns = [
     re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', TemplateView.as_view(template_name='main.html'), name='main'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
