0d8d43526db3e556a9049d2690677bb2b853bbdd
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2022-12-25 05:14:08 +0400

    overflow

--- frontend/src/navigation/Mainpage.tsx
+++ frontend/src/navigation/Mainpage.tsx
@@ -1,8 +1,8 @@
 const Mainpage = () => {
-    
+
     return (
         <main class="px-3 text-center">
-            <p  style="margin-bottom:3cm"></p>
+            <p style="margin-bottom:3cm"></p>
             <h1>PySchool</h1>
             <br></br>
             <p class="lead">
@@ -10,7 +10,11 @@ const Mainpage = () => {
                 <br></br>
                 для школьников
             </p>
+            <footer class="fixed-bottom text-center">
+                <p><a href="https://t.me/a_s_shuv" target="_blank" class="text-white">a_s_shuv</a></p>
+            </footer>
         </main>
+
     );
 };
 
