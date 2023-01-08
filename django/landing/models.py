8e40786269fe2b28ce6b32a2726b4427f1ad70de
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-01-08 08:55:16 +0400

    lead model

--- django/landing/models.py
+++ django/landing/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Parent(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.last_name
+
+
+class Children(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30)
+    class_number = models.PositiveIntegerField()
+
+    def __str__(self) -> str:
+        return self.last_name
+
+
+class Lead(models.Model):
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE)
+    children = models.ForeignKey(Children, on_delete=models.CASCADE)
+    email = models.EmailField()
+    phone = models.CharField(max_length=30)
+    extra = models.TextField()
+
+    def __str__(self) -> str:
+        return self.email
+
